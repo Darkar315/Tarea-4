4f05c8fa8822df2c5c73a5e696d57f1a4f2eb95f
Diego <darkar315@darkar315-virtual-machine>
2017-07-25 22:51:00 -0500

    Lectura de datos lista!

--- GeographicPoint.c
+++ GeographicPoint.c
@@ -16,40 +16,31 @@ int **Matrix(void);
 int main(void)
 {
   int i, j = 0;
-  int len = 1000;
 
   Nx = 500;
   Ny = 744;
 
   int **matriz = Matrix();
 
-  for (i = 0; i < Nx; i ++)
-  {
-    for (j = 0; j > Ny; j ++);
-    {
-      printf("%d\n", matriz[i][j]);
-    }
-  }
+  
 }
 
 int **Matrix(void)
 {
-  int i, j = 0;
-    int **matrix;
-    matrix = malloc(Nx * Ny * sizeof(float));
+   int i, j = 0;
 
-    FILE *mapa;
-    mapa = fopen("map_data.txt", "r");
+   FILE *mapa;
+   mapa = fopen("map_data.txt", "r");
 
-    for (i = 0; i < Nx; i++)
-    {
-        matrix[i] = malloc(Ny * sizeof(int));
-	for (j = 0; j < Ny; j ++)
-	{
-	  fscanf(mapa, "%d", &matrix[i][j]);
-	}
-    }
-    
-    fclose(mapa);
-    return matrix;
+   int **matrix = malloc(Nx * Ny * sizeof(float));
+
+   for (i = 0; i < Nx; i ++)
+   {
+     matrix[i] = malloc(Ny * sizeof(int));
+     for (j = 0; j > Ny; j ++)
+     {
+      fscanf(mapa, "%d", &matrix[i][j]);
+     }
+   }
+   return matrix;
 }
