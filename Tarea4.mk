8172772e1bc2f84a80456307e7070ecf27e4bf5c
Diego <darkar315@darkar315-virtual-machine>
2017-07-27 20:33:35 -0500

    Make fila

---
+++ Tarea4.mk
@@ -0,0 +1,12 @@
+# MakeFile para la tarea 4.
+PuntoNemo.pdf : Plots.py datos.txt
+	python Plots.py
+
+datos.txt : GeographicPoint.x
+	./GeographicPoint.x
+
+GeographicPoint.x : GeographicPoint.c
+	cc GeographicPoint.c -o GeographicPoint.x -lm
+
+clean :
+	rm datos.txt *.pdf *.x
